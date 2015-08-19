3ec32422358c738dd8a02288949e23dd318c97fb
Michael Henry <froderick@gmail.com>
2015-08-19 10:29:34 -0400

    experimenting with interop

---
+++ clj/src/java/org/funtastic/dist/Distributor.java
@@ -0,0 +1,34 @@
+package org.funtastic.dist;
+
+import java.util.Set;
+
+/**
+ * A mechanism for coordinated distribution of hash buckets across a networked
+ * cluster of computers.
+ *
+ * <p>Implementations of this interface should be thread safe.</p>
+ *
+ * @see RabbitDistributor
+ */
+public interface Distributor {
+
+    /**
+     * Returns the set of buckets currently available to a distributor
+     * instance.
+     *
+     * <p>This method may return an empty set if no buckets are yet available.
+     * It may block, depending on the implementation.</p>
+     */
+    Set<String> buckets();
+
+    /**
+     * Releases the current set of buckets back to the cluster.
+     *
+     * <p>This method is a way of indicating that the client is done with
+     * the current set of buckets. The distributor is responsible
+     * for fetching the next set of buckets. This method may block,
+     * depending on the implementation.</p>
+     */
+    void release(Set<String> buckets);
+}
+
---
+++ clj/src/java/org/funtastic/dist/rabbit/Service.java
@@ -0,0 +1,70 @@
+package org.funtastic.dist.rabbit;
+
+import com.rabbitmq.client.Connection;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Defines the parameters for configuring and managing a rabbit bucket distributor
+ * in a java-friendly way.
+ */
+public interface Service {
+
+    void setConnection(Connection conn);
+
+    /**
+     * This is used as a base prefix to name all the rabbitmq configuration related
+     * to the bucket distributor. This is required.
+     */
+    void setName(String name);
+
+    /**
+     * This is used to initialize the bucket consumer queue on startup if it is
+     * not already initialized. This is required.
+     */
+    void setDefaultBuckets(Set<String> defaultBuckets);
+
+    /**
+     * The scheduler thread pool used to perform periodic actions to maintain
+     * the list of peers and the consumer partition size. This is required.
+     */
+    void setScheduler(ScheduledExecutorService scheduler);
+
+    /**
+     * The period at which the distributor announces itself to its peers and
+     * maintains its up-to-date set of known-healthy peers. Defaults to 1 minute.
+     */
+    void setPeersPeriod(long period);
+    void setPeersUnits(TimeUnit unit);
+
+    /**
+     * If the distributor hasn't heard from a peer after this period, it considers
+     * the peer to be gone and expires it from the set of known peers. Defaults to
+     * 2 minutes.
+     */
+    void setExpirationPeriod(long period);
+    void setExpirationUnits(TimeUnit unit);
+
+    /**
+     * The initial delay before the distributor begins to examine its known peers
+     * to determine the bucket partition size it should use. Defaults to 5 seconds.
+     */
+    void setPartitionDelay(long delay);
+
+    /**
+     * The period at which the distributor examines its known peers and updates its
+     * bucket partition size to match. Defaults to 5 seconds.
+     */
+    void setPartitionPeriod(long period);
+    void setPartitionUnits(TimeUnit unit);
+
+    void start();
+
+    void stop();
+}
+
