6a6ed595e1510943e9b43e8a77016f2189e1f4de
froderick <froderick@gmail.com>
2015-08-29 16:44:57 -0400

    Trying out the prismatic schema stuff on the api

--- hyrax/src/clojure/hyrax/dist/api.clj
+++ hyrax/src/clojure/hyrax/dist/api.clj
@@ -1,14 +1,24 @@
-(ns hyrax.dist.api)
+(ns hyrax.dist.api
+  "A mechanism for coordinated distribution of hash buckets."
+  (:require [schema.core :as s]))
 
-(defprotocol Distributor "A mechanism for coordinated distribution of
-                                hash buckets."
+(defprotocol Distributor 
+  (acquire-buckets* [this] )
+  (release-buckets* [this buckets] ))
 
-  (acquire-buckets! [this] "Returns the set of buckets currently available to a
-                   distributor instance. This function may return an empty set
-                   if no buckets are yet available. Must not block.")
+(s/defn ^:always-validate acquire-buckets! :- #{String} 
+  "Returns the set of buckets currently available to a
+   distributor instance. This function may return an empty set
+   if no buckets are yet available. Must not block."
+  [this]
+  (acquire-buckets* this))
 
-  (release-buckets! [this buckets] "Releases the current set of buckets back to the
-                           cluster.  This method is a way of indicating that
-                           the client is done with the current set of buckets.
-                           The distributor is responsible for fetching the next
-                           set of buckets. Must not block."))
+(s/defn ^:always-validate release-buckets! :- nil 
+  "Releases the current set of buckets back to the
+   cluster.  This method is a way of indicating that
+   the client is done with the current set of buckets.
+   The distributor is responsible for fetching the next
+   set of buckets. Must not block."
+  [this buckets :- #{String}]
+  (release-buckets* this buckets))
+
